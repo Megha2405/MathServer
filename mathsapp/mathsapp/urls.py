ebde8f42d11d79b9c2519dfca61b88dc4dffbf99
megha2405 <meghasivan2405@gmail.com>
2026-02-15 13:23:23 +0530

    Success

---
+++ mathsapp/mathsapp/urls.py
@@ -0,0 +1,11 @@
+
+from django.contrib import admin
+from django.urls import path, include
+from mathapp import views
+
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('', views.gst_amt_calculator, name='gst_amt'),
+]
+
